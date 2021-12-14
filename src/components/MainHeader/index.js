473e302bdddd31dfe1ec4c9e22951b955c3e8c6d
Mark Rodriguez <MRodriguez@homerbonner.com>
2021-12-14 15:32:49 -0500

    Added cart side slide menu

--- src/components/MainHeader/index.js
+++ src/components/MainHeader/index.js
@@ -1,9 +1,9 @@
 // import '../MainHeader/MainHeader.css'
-import { useState } from 'react';
-import styled from 'styled-components'
+import { useState } from "react";
+import styled from "styled-components";
 
-// COMPONENTS 
-import { Button, Navbar, Container, Offcanvas } from 'react-bootstrap';
+// COMPONENTS
+import { Button, Navbar, Container, Offcanvas } from "react-bootstrap";
 
 // IMAGES
 
@@ -11,23 +11,31 @@ import { Button, Navbar, Container, Offcanvas } from 'react-bootstrap';
 import rosterLogo2 from "../../imgs/logo/roosterE.png";
 import rosterLogo from "../../imgs/logo/roosterG.png";
 
-
 // ICONS
-import { BsFillArrowRightCircleFill, BsFillCartFill, BsFacebook, BsInstagram, BsYoutube, BsFillEnvelopeFill, BsTwitter } from "react-icons/bs";
+import {
+  BsFillArrowRightCircleFill,
+  BsFillCartFill,
+  BsFacebook,
+  BsInstagram,
+  BsYoutube,
+  BsFillEnvelopeFill,
+  BsTwitter,
+} from "react-icons/bs";
 
 // CSS
 const Header = styled(Navbar)`
- display: flex;
- justify-content: space-between;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
   /* background-color:#4a4a4a;  */
-  background-color:#2f3030;
+  background-color: #2f3030;
   /* background-color: #2e80d1;  */
   /* margin: 10px; */
   padding: 13px;
-  -webkit-box-shadow: 1px 7px 27px 3px rgba(0,0,0,0.53); 
-box-shadow: 1px 7px 27px 3px rgba(0,0,0,0.53);
+  -webkit-box-shadow: 1px 7px 27px 3px rgba(0, 0, 0, 0.53);
+  box-shadow: 1px 7px 27px 3px rgba(0, 0, 0, 0.53);
 
-& img:hover {
+  & img:hover {
     /* transform: scale(1.07); */
     /* transform: rotateX(10deg); */
     transform: rotate(1turn);
@@ -39,7 +47,7 @@ box-shadow: 1px 7px 27px 3px rgba(0,0,0,0.53);
     transition: all 1s ease;
   }
 
-/* & h1:hover {
+  /* & h1:hover {
  
  text-decoration: wavy;
  -webkit-transition: all 0.5s ease;  
@@ -51,152 +59,192 @@ box-shadow: 1px 7px 27px 3px rgba(0,0,0,0.53);
      */
 `;
 
-const Menu = styled('h2')`
+const Menu = styled("h2")`
   color: white;
-  background-color:transparent; 
+  background-color: transparent;
   cursor: pointer;
-  text-decoration:none;   
-  font-family:"menuFont";
+  text-decoration: none;
+  font-family: "menuFont";
   font-size: 30px;
   margin-left: 6px;
 `;
 
-const BrandLink = styled('a')`
-text-decoration: none;
-`
+const SubSideMenu = styled("div")`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: space-around;
+`;
 
-const BrandIcon = styled('img')`
-width:70px;
-height: 75px;
-background-color:transparent;
-/* border: white 4px dotted; */
-/* border: white 4px transparent; */
-border-radius: 50%;
-padding: 7px;
-margin-bottom:8px;
-margin-right: 3px;
-`
-const MenuHeaderIcon = styled('img')`
-width:160px;
-height: 195px;
-background-color:transparent;
-color:white;
-padding: 4px;
-margin-top: 40px;
-margin-left: 50px;
-margin-bottom:8px;
+const BrandLink = styled("a")`
+  text-decoration: none;
+`;
 
-`
+const BrandIcon = styled("img")`
+  width: 70px;
+  height: 75px;
+  background-color: transparent;
+  /* border: white 4px dotted; */
+  /* border: white 4px transparent; */
+  border-radius: 50%;
+  padding: 7px;
+  margin-bottom: 8px;
+  margin-right: 3px;
+`;
+const MenuHeaderIcon = styled("img")`
+  width: 160px;
+  height: 195px;
+  background-color: transparent;
+  color: white;
+  padding: 4px;
+  margin-top: 40px;
+  margin-left: 50px;
+  margin-bottom: 8px;
+`;
 
-const MenuDivider = styled('hr')`
-color:white;
-font-size: 40px;
-
-`
-const BrandText = styled('h1')`
-    font-family:"brandFont";
-    color:white;
-    letter-spacing: 2px;
-    border: white 4.2px dotted;  
-    padding: 6px 6px 0px 6px;
-    margin-right: 6px;
-    margin-top: 4px;
-    font-size: 48px;
-`
+const MenuDivider = styled("hr")`
+  color: white;
+  font-size: 40px;
+`;
+const BrandText = styled("h1")`
+  font-family: "brandFont";
+  color: white;
+  letter-spacing: 2px;
+  border: white 4.2px dotted;
+  padding: 6px 6px 0px 6px;
+  margin-right: 6px;
+  margin-top: 4px;
+  font-size: 48px;
+`;
 
 const SlideMenuContainer = styled(Offcanvas)`
-background-color:#2f3030;
-color: white;
+  background-color: #2f3030;
+  color: white;
+`;
 
-`
+const MenuFlex = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-content: stretch;
+`;
 
-const MenuFlex = styled('div')`
-display: flex;
-flex-direction: column;
-justify-content:center;
-align-items: center;
-align-content: stretch;
+const MenuItem = styled("a")`
+  text-decoration: none;
+  color: white;
+  font-size: 28px;
+  padding-top: 10.5px;
+`;
 
-`
+const SocialMediaBar = styled("div")`
+  color: white;
+  /* background-color:red; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: space-around;
+  margin-top: 10px;
+`;
 
-const MenuItem = styled('a')`
-text-decoration: none;
-color: white;
-font-size: 28px;
-padding-top: 10.5px;
-
-
-`
-
-const SocialMediaBar = styled('div')`
-color:white;
-/* background-color:red; */
-display:flex;
-justify-content:center;
-align-items: center;
-align-content: space-around;
-margin-top: 10px;
-`
-
-const SocialMediaItem = styled('a')`
-text-decoration: none;
-padding: 12px;
-font-size: 30px;
-color:white;
-
-`
+const SocialMediaItem = styled("a")`
+  text-decoration: none;
+  padding: 12px;
+  font-size: 30px;
+  color: white;
+`;
 
 function MainHeader() {
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    return (
-        <>
-            <Header fixed="top" >
-                {/* Maybe put a menu Icon - Roster's Head at the end of the "Menu" Left btton */}
-                <div>
-                    <Menu onClick={handleShow}>Menu < BsFillArrowRightCircleFill /> | Cart <BsFillCartFill /></Menu>
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-                </div>
-                {/* <Menu onClick={handleShow}>Menu < BrandIcon src={rosterLogo} /> </Menu> */}
-                <SlideMenuContainer show={show} onHide={handleClose}>
+  const [showB, setShowB] = useState(false);
 
-                    <Offcanvas.Body>
-                        <MenuFlex>
-                            <Offcanvas.Header closeButton>
-                                <Offcanvas.Title><MenuHeaderIcon src={rosterLogo2} /></Offcanvas.Title>
-                            </Offcanvas.Header>
-                            <p>__________</p>
-                            <MenuItem href="">SHOP</MenuItem>
-                            <MenuItem href="#">ARTISTS</MenuItem>
-                            <MenuItem href="#">CART</MenuItem>
-                            <MenuItem href="#">ABOUT</MenuItem>
-                            <MenuItem href="#">SUPPORT</MenuItem>
-                        </MenuFlex>
-                        <SocialMediaBar>
-                            <SocialMediaItem href="#"><BsFacebook /></SocialMediaItem>
-                                <SocialMediaItem href="#">  <BsInstagram /></SocialMediaItem>
-                                    <SocialMediaItem href="#">  <BsYoutube /></SocialMediaItem>
-                                        <SocialMediaItem href="#">  <BsFillEnvelopeFill /></SocialMediaItem>
-                                            <SocialMediaItem href="#">   <BsTwitter /></SocialMediaItem >
+  const handleCloseB = () => setShowB(false);
+  const handleShowB = () => setShowB(true);
 
-                                        
+  return (
+    <>
+      <Header fixed="top">
+        {/* Maybe put a menu Icon - Roster's Head at the end of the "Menu" Left btton */}
+        <SubSideMenu >
+          <Menu onClick={handleShow}>
+            Menu  <BsFillArrowRightCircleFill />
+          </Menu>
+          <Menu onClick={handleShowB}>
+            | Cart <BsFillCartFill />
+          </Menu>
+        </SubSideMenu >
 
-                                            </SocialMediaBar>
-                                        </Offcanvas.Body>
-                                    </SlideMenuContainer>
+        {/* OFFSIDE-LEFT-SLIDE-MENU */}
+        <SlideMenuContainer show={show} onHide={handleClose}>
+          <Offcanvas.Body>
+            <MenuFlex>
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title>
+                  <MenuHeaderIcon src={rosterLogo2} />
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <p>__________</p>
+              <MenuItem href="">SHOP</MenuItem>
+              <MenuItem href="#">ARTISTS</MenuItem>
+              <MenuItem href="#">CART</MenuItem>
+              <MenuItem href="#">ABOUT</MenuItem>
+              <MenuItem href="#">SUPPORT</MenuItem>
+            </MenuFlex>
+            <SocialMediaBar>
+              <SocialMediaItem href="#">
+                <BsFacebook />
+              </SocialMediaItem>
+              <SocialMediaItem href="#">
+                {" "}
+                <BsInstagram />
+              </SocialMediaItem>
+              <SocialMediaItem href="#">
+                {" "}
+                <BsYoutube />
+              </SocialMediaItem>
+              <SocialMediaItem href="#">
+                {" "}
+                <BsFillEnvelopeFill />
+              </SocialMediaItem>
+              <SocialMediaItem href="#">
+                {" "}
+                <BsTwitter />
+              </SocialMediaItem>
+            </SocialMediaBar>
+          </Offcanvas.Body>
+        </SlideMenuContainer>
 
-                                    {/*** DIFFERENT LOGO IDEAS */}
-                                    {/* <BrandLink href="https://www.nhl.com/"><BrandText>GALLO-OCHO</BrandText></BrandLink> */}
-                                    <BrandLink href="https://www.nhl.com/"><BrandText>GALLO<BrandIcon src={rosterLogo} />OCHO</BrandText></BrandLink>
-                                </Header>
+        {/* OFFSIDE-RIGHT-SLIDE-MENU */}
+        <SlideMenuContainer placement={"end"} show={showB} onHide={handleCloseB}>
+          <Offcanvas.Body>
+            <MenuFlex>
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title>
+                <BsFillCartFill />CART
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <p>__________</p>
+              <MenuItem href="">ITEMS:</MenuItem>
+            </MenuFlex>
+          </Offcanvas.Body>
+        </SlideMenuContainer>
 
-                            </>
-
-
-                            );
+        {/*** DIFFERENT LOGO IDEAS */}
+        {/* <BrandLink href="https://www.nhl.com/"><BrandText>GALLO-OCHO</BrandText></BrandLink> */}
+        <BrandLink href="https://www.nhl.com/">
+          <BrandText>
+            GALLO
+            <BrandIcon src={rosterLogo} />
+            OCHO
+          </BrandText>
+        </BrandLink>
+      </Header>
+    </>
+  );
 }
 
-                            export default MainHeader;
-
+export default MainHeader;
